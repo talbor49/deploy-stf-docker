a9f6d5a2c82d9e02bb3c44c91ad92f07211d6ea2
tal <tal@localhost.localdomain>
2020-09-24 18:59:37 +0300

    Make it work and advance to devicefarmer (from old openstf)

--- deploy_stf.sh
+++ deploy_stf.sh
@@ -13,17 +13,39 @@
 # 
 #######################################################################################
 
+display_usage() { 
+	echo "This script must be run with super-user privileges." 
+  echo -e "\nUsage: $0 [arguments] \n" 
+} 
+# Get exported IP Adreess 
+# if less than two arguments supplied, display usage 
+if [  $# -lt 1 ] 
+then 
+  display_usage
+  exit 1
+fi
+
+# check whether user had supplied -h or --help . If yes display usage 
+if [[ ( $# == "--help") ||  $# == "-h" ]] 
+then 
+  display_usage
+  exit 0
+fi 
+
+# display usage if the script is not run as root user 
+if [[ $USER != "root" ]]; then 
+  echo "This script must be run as root!" 
+  exit 1
+fi 
+ 
 NETNAME=""
 NETWORK_INTERFACES=$(ls /sys/class/net)
 
 DNS_ADDRESS=$(nmcli device show ${NETNAME}|grep "IP4\.DNS\[1\]"|awk '{print $2}')
 echo "DNS Address: ${DNS_ADDRESS}"
 
-# Get exported IP Adreess 
-[ ! -z "$(echo ${NETWORK_INTERFACES} | grep "wlo1")" ]&&NETNAME="wlo1"
-[ ! -z "$(echo ${NETWORK_INTERFACES} | grep "eno1")" ]&&NETNAME="eno1"
-IP_ADDRESS=$(ifconfig ${NETNAME}|grep "inet "|awk -F: '{print $2}'|awk '{print $1}')
-[ $# -gt 0 ]&&IP_ADDRESS=$1
+
+IP_ADDRESS=$1
 echo "IP ADDRESS: ${IP_ADDRESS}"
 
 check_return_code() {
@@ -57,7 +79,7 @@ prepare() {
   apt-get install -y docker.io
   assert_run_ok
 
-  docker pull openstf/stf 
+  docker pull devicefarmer/stf 
   assert_run_ok
 
   #docker pull sorccu/adb 
@@ -66,7 +88,7 @@ prepare() {
   docker pull rethinkdb 
   assert_run_ok
 
-  docker pull openstf/ambassador 
+  docker pull devicefarmer/ambassador 
   assert_run_ok
 
   docker pull nginx
@@ -82,9 +104,14 @@ prepare() {
 
 }
 
+exit 0
+
+
 if [ ! -e env.ok ]; then
   prepare
 fi
+
+
 
 # start local adb server
 echo "start adb server"
@@ -105,77 +132,77 @@ check_return_code
 # create tables 
 echo "start docker container: stf-migrate"
 docker rm -f stf-migrate
-docker run -d --name stf-migrate --net host openstf/stf stf migrate
+docker run -d --name stf-migrate --net host devicefarmer/stf stf migrate
 check_return_code
 
 # create storage components 
 echo "start docker container: storage-plugin-apk-3300"
 docker rm -f storage-plugin-apk-3300
-docker run -d --name storage-plugin-apk-3300 -p 3300:3000 --dns ${DNS_ADDRESS}  openstf/stf stf storage-plugin-apk  --port 3000 --storage-url http://${IP_ADDRESS}/
+docker run -d --net host --name storage-plugin-apk-3300 --dns ${DNS_ADDRESS}  devicefarmer/stf stf storage-plugin-apk --port 3300 --storage-url http://${IP_ADDRESS}/
 check_return_code
 
 echo "start docker container: storage-plugin-image-3400"
 docker rm -f storage-plugin-image-3400
-docker run -d --name storage-plugin-image-3400 -p 3400:3000 --dns ${DNS_ADDRESS}  openstf/stf stf storage-plugin-image  --port 3000 --storage-url http://${IP_ADDRESS}/
+docker run -d --net host --name storage-plugin-image-3400 --dns ${DNS_ADDRESS}  devicefarmer/stf stf storage-plugin-image  --port 3400 --storage-url http://${IP_ADDRESS}/
 check_return_code  
 
 echo "start docker container: storage-temp-3500"
 docker rm -f storage-temp-3500
-docker run -d --name storage-temp-3500 -v /mnt/storage:/data -p 3500:3000 --dns ${DNS_ADDRESS}  openstf/stf stf storage-temp  --port 3000 --save-dir /home/stf
+docker run -d --name storage-temp-3500 -v /mnt/storage:/data -p 3500:3000 --dns ${DNS_ADDRESS}  devicefarmer/stf stf storage-temp  --port 3000 --save-dir /home/stf
 check_return_code
 
 # tri-proxy
 echo "start docker container: triproxy-app"
 docker rm -f triproxy-app
-docker run -d --name triproxy-app --net host openstf/stf stf triproxy app --bind-pub "tcp://*:7150" --bind-dealer "tcp://*:7160" --bind-pull "tcp://*:7170"
+docker run -d --name triproxy-app --net host devicefarmer/stf stf triproxy app --bind-pub "tcp://*:7150" --bind-dealer "tcp://*:7160" --bind-pull "tcp://*:7170"
 check_return_code
 
 echo "start docker container: triproxy-dev"
 docker rm -f triproxy-dev
-docker run -d --name triproxy-dev --net host openstf/stf stf triproxy dev --bind-pub "tcp://*:7250" --bind-dealer "tcp://*:7260" --bind-pull "tcp://*:7270"
+docker run -d --name triproxy-dev --net host devicefarmer/stf stf triproxy dev --bind-pub "tcp://*:7250" --bind-dealer "tcp://*:7260" --bind-pull "tcp://*:7270"
 check_return_code
 
 # auth
 echo "start docker container: stf-auth-3200"
 docker rm -f stf-auth-3200
-docker run -d --name stf-auth-3200 -e "SECRET=YOUR_SESSION_SECRET_HERE" -p 3200:3000 --dns ${DNS_ADDRESS} openstf/stf stf auth-mock --port 3000 --app-url http://${IP_ADDRESS}/
+docker run -d --name stf-auth-3200 --net host -e "SECRET=YOUR_SESSION_SECRET_HERE" --dns ${DNS_ADDRESS} devicefarmer/stf stf auth-mock --port 3200 --app-url http://${IP_ADDRESS}/
 check_return_code
 
 # api 
 echo "start docker container: stf-api"
 docker rm -f stf-api
-docker run -d --name stf-api --net host -e "SECRET=YOUR_SESSION_SECRET_HERE"  openstf/stf stf api --port 3700 --connect-sub tcp://${IP_ADDRESS}:7150  --connect-push tcp://${IP_ADDRESS}:7170
+docker run -d --name stf-api --net host -e "SECRET=YOUR_SESSION_SECRET_HERE"  devicefarmer/stf stf api --port 3700 --connect-push tcp://${IP_ADDRESS}:7150 --connect-sub tcp://${IP_ADDRESS}:7150  --connect-push-dev tcp://${IP_ADDRESS}:7170 --connect-sub-dev tcp://${IP_ADDRESS}:7170
 check_return_code
 
 # stf APP
 echo "start docker container: stf-app-3100"
 docker rm -f stf-app-3100
-docker run -d --name stf-app-3100 --net host -e "SECRET=YOUR_SESSION_SECRET_HERE" -p 3100:3000 openstf/stf stf app --port 3100 --auth-url http://${IP_ADDRESS}/auth/mock/ --websocket-url http://${IP_ADDRESS}/
+docker run -d --name stf-app-3100 --net host -e "SECRET=YOUR_SESSION_SECRET_HERE" -p 3100:3000 devicefarmer/stf stf app --port 3100 --auth-url http://${IP_ADDRESS}/auth/mock/ --websocket-url http://${IP_ADDRESS}/
 check_return_code
 
 
 # processor
 echo "start docker container: stf-processor"
 docker rm -f stf-processor
-docker run -d --name stf-processor --net host openstf/stf stf processor stf-processor.service --connect-app-dealer tcp://${IP_ADDRESS}:7160 --connect-dev-dealer tcp://${IP_ADDRESS}:7260
+docker run -d --name stf-processor --net host devicefarmer/stf stf processor stf-processor.service --connect-app-dealer tcp://${IP_ADDRESS}:7160 --connect-dev-dealer tcp://${IP_ADDRESS}:7260
 check_return_code
 
 # websocket
 echo "start docker container: websocket"
 docker rm -f websocket
-docker run -d --name websocket -e "SECRET=YOUR_SESSION_SECRET_HERE" --net host openstf/stf stf websocket --port 3600 --storage-url http://${IP_ADDRESS}/ --connect-sub tcp://${IP_ADDRESS}:7150 --connect-push tcp://${IP_ADDRESS}:7170
+docker run -d --name websocket -e "SECRET=YOUR_SESSION_SECRET_HERE" --net host devicefarmer/stf stf websocket --port 3600 --storage-url http://${IP_ADDRESS}/ --connect-sub tcp://${IP_ADDRESS}:7150 --connect-push tcp://${IP_ADDRESS}:7170
 check_return_code
 
 # reaper
 echo "start docker container: reaper"
 docker rm -f reaper
-docker run -d --name reaper --net host openstf/stf stf reaper dev --connect-push tcp://${IP_ADDRESS}:7270 --connect-sub tcp://${IP_ADDRESS}:7150 --heartbeat-timeout 30000
+docker run -d --name reaper --net host devicefarmer/stf stf reaper dev --connect-push tcp://${IP_ADDRESS}:7270 --connect-sub tcp://${IP_ADDRESS}:7150 --heartbeat-timeout 30000
 check_return_code
 
 # provider
 echo "start docker container: provider-${HOSTNAME}"
 docker rm -f provider1
-docker run -d --name provider1 --net host openstf/stf stf provider --name "provider-${HOSTNAME}" --connect-sub tcp://${IP_ADDRESS}:7250 --connect-push tcp://${IP_ADDRESS}:7270 --storage-url http://${IP_ADDRESS} --public-ip ${IP_ADDRESS} --min-port=15000 --max-port=25000 --heartbeat-interval 20000 --screen-ws-url-pattern "ws://${IP_ADDRESS}/d/floor4/<%= serial %>/<%= publicPort %>/"
+docker run -d --name provider1 --net host devicefarmer/stf stf provider --name "provider-${HOSTNAME}" --connect-sub tcp://${IP_ADDRESS}:7250 --connect-push tcp://${IP_ADDRESS}:7270 --storage-url http://${IP_ADDRESS} --public-ip ${IP_ADDRESS} --min-port=15000 --max-port=25000 --heartbeat-interval 20000 --screen-ws-url-pattern "ws://${IP_ADDRESS}/d/floor4/<%= serial %>/<%= publicPort %>/"
 check_return_code
 
 
